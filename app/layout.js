dc26775cae1cc670d01f906fe0e730dbe0bfabb1
pomppa <tuomas.anakkala@gmail.com>
2023-02-27 21:15:11 +0200

    init

---
+++ app/layout.js
@@ -0,0 +1,19 @@
+import { Source_Code_Pro } from "next/font/google";
+
+const source_code_pro = Source_Code_Pro({
+  subsets: ["latin"],
+  display: "swap",
+});
+
+export const metadata = {
+  title: "bio",
+  description: "bio @pomppa",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en" className={source_code_pro.className}>
+      <body>{children}</body>
+    </html>
+  );
+}
