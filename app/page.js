47be88ba23ca602768d43677d074c529f27a18ec
pomppa <tuomas.anakkala@gmail.com>
2023-03-04 12:27:01 +0200

    pages

--- app/page.js
+++
@@ -1,4 +0,0 @@
-// This file maps to the index route (/)
-export default function Page() {
-  return <h1>hello 👋 ツ </h1>;
-}
---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <>
+      <h2>👋 hello ツ </h2>
+    </>
+  );
+}
