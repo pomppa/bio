de1f3a77d0ab1b5e67ec6a8d210b4347f98453cb
pomppa <tuomas.anakkala@gmail.com>
2023-03-11 13:13:57 +0200

    spotify client creds

---
+++ app/spotify/page.tsx
@@ -0,0 +1,34 @@
+import { PlaylistRootObject } from "../../lib/types";
+
+export async function getData() {
+  const res = await fetch(
+    `https://api.spotify.com/v1/users/${process.env.SPOTIFY_USER_ID}/playlists`,
+    {
+      headers: {
+        Authorization: "Bearer " + process.env.SPOTIFY_ACCESS_TOKEN,
+      },
+    }
+  );
+  const json: PlaylistRootObject = await res.json();
+  return json;
+}
+export default async function Page() {
+  const data = await getData();
+  return (
+    <>
+      <h1>spotify</h1>
+
+      {data.items.map((x) => {
+        return (
+          <>
+            <ul>
+              <li>
+                <a href={x.external_urls.spotify}>{x.external_urls.spotify}</a>
+              </li>
+            </ul>
+          </>
+        );
+      })}
+    </>
+  );
+}
